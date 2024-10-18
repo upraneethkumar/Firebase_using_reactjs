47081ea1c246318d8916f8eb6f4f5be686bd8224
upraneethkumar <upraneeth24@gmail.com>
2024-10-18 21:16:26 +0530

    1

---
+++ firebase-course/src/config/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDUUyDtXImyVqAUqVK5f3Sr-w2EwuYDQe8",
+  authDomain: "fir-course-d50f7.firebaseapp.com",
+  projectId: "fir-course-d50f7",
+  storageBucket: "fir-course-d50f7.appspot.com",
+  messagingSenderId: "863400658680",
+  appId: "1:863400658680:web:4fe9636dd234b758ed5d0d",
+  measurementId: "G-LNHN19GCDF"
+};
+
+const app = initializeApp(firebaseConfig);
