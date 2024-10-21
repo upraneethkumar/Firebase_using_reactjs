297fc7b274c979434ec3b4d612be8bb5fca49bea
upraneethkumar <upraneeth24@gmail.com>
2024-10-22 00:27:38 +0530

    2

--- firebase-course/src/config/firebase.js
+++ firebase-course/src/config/firebase.js
@@ -1,4 +1,6 @@
 import { initializeApp } from "firebase/app";
+import {getAuth } from 'firebase/auth'
+
 
 const firebaseConfig = {
   apiKey: "AIzaSyDUUyDtXImyVqAUqVK5f3Sr-w2EwuYDQe8",
@@ -11,3 +13,4 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app)
